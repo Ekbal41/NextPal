15f2ac868457b8cd3648b1652232b277932eb406
Ekbal41 <mdasifekbalshagor@gmail.com>
2023-05-16 13:18:52 +0600

    lol

--- app/page.js
+++ app/page.js
@@ -1,13 +1,23 @@
-"use client"
+"use client";
 import ItemCard from "@/components/ItemCard";
 import Navbar from "@/components/Navbar";
+import { Grid } from "@chakra-ui/react";
 export default function Home() {
   return (
     <>
       <Navbar />
-      <div>
+      <Grid 
+        templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)", lg: "repeat(3, 1fr)", xl: "repeat(4, 1fr)" }}
+      >
         <ItemCard />
-      </div>
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+      </Grid>
     </>
   );
 }
